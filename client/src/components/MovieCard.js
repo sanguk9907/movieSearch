6cdf43441e056460d58f750c5b28dfa6301ba55c
sanguk9907 <tkddnr6079@naver.com>
2022-09-29 22:20:16 +0900

    제목 검색 구현

--- client/src/components/MovieCard.js
+++ client/src/components/MovieCard.js
@@ -6,11 +6,17 @@ function MovieCard(props) {
   const director = props.data.director;
   const actor = props.data.actor;
   const userRating = props.data.userRating;
+  const pubDate = props.data.pubDate;
+  const link = props.data.link;
+  const subtitle = props.data.subtitle;
+
   return (
     <div className="moviecard-wrap">
       <div className="movie-imgbox">
         <img src={image} alt={title} />
-        <h3 className="movie-title">{title}</h3>
+        <h3 className="movie-title">
+          {title}({pubDate})<p className="subtitle">{subtitle}</p>
+        </h3>
       </div>
       <p className="director">
         감독 : <b>{director === "" ? "정보없음" : director}</b>
@@ -21,6 +27,9 @@ function MovieCard(props) {
       <p>
         평점 : <b>{userRating === "0.00" ? "정보없음" : userRating}</b>
       </p>
+      <a href={link} target="blank">
+        자세히 보기
+      </a>
     </div>
   );
 }
--- client/src/components/SearchBox.js
+++ client/src/components/SearchBox.js
@@ -1,17 +1,25 @@
 import React from "react";
 
-function SearchBox(props) {
+function SearchBox({ searchInputValue }) {
   const [search, setSearch] = React.useState("");
   return (
     <div className="searchBox-wrap">
-      <input
-        type="text"
-        value={search}
-        placeholder="검색어를 입력해주세요"
-        onChange={(e) => {
-          setSearch(e);
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          searchInputValue(search);
         }}
-      />
+      >
+        <input
+          className="search-input"
+          type="text"
+          value={search}
+          placeholder="검색어를 입력해주세요"
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+        />
+      </form>
     </div>
   );
 }
