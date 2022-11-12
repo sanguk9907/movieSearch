1d0b74cfab9c2b9817b10e0922c3cd7326db4c11
이상욱 <sanguk6079@gmail.com>
2022-11-12 22:01:46 +0900

    이미지 등록은 성공 출력은 실패..

--- client/src/App.js
+++ client/src/App.js
@@ -80,4 +80,37 @@ function App() {
   );
 }
 
+function Test() {
+  axios.defaults.withCredentials = true;
+  const file = React.useRef();
+  const [images, setImages] = React.useState({});
+  const [profileImage, setProfileImage] = React.useState("");
+
+  const handleChange = (e) => {
+    setImages(e.target.files[0]);
+  };
+
+  const submitFile = async () => {
+    const form = new FormData();
+    form.append("file", images);
+    await axios
+      .post("http://localhost:5000/test", form, {
+        "Content-Type": "multipart/form-data",
+      })
+      .then(({ data }) => {
+        setProfileImage(data.filename);
+      });
+  };
+
+  return (
+    <div>
+      <input ref={file} type="file" onChange={handleChange} />
+      <button onClick={submitFile} type="button">
+        전송
+      </button>
+      <img src={`./img/${profileImage}`} alt="profile"></img>
+    </div>
+  );
+}
+
 export default App;
