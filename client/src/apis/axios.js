4a588d31360fe3bd8bc306117865742dd87bc7e9
이상욱 <sanguk9907>
2022-11-20 15:14:52 +0900

    a

--- client/src/apis/axios.js
+++ client/src/apis/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 const key = "3cf148810eae178af2afb1a072cfe76d";
 const instance = axios.create({
-  baseURL: "http://52.196.233.251:5000",
-  // baseURL: "http://localhost:5000",
+  // baseURL: "http://52.196.233.251:5000",
+  baseURL: "http://localhost:5000",
   params: {
     api_key: key,
   },
